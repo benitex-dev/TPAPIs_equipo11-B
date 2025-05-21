0f9470b3c4288cf420b27eafaed65c59684bc00a
benitex-dev <benitezjorgedev@gmail.com>
2025-05-21 18:47:04 -0300

    Se agrega capa de dominio y capa de negocio

---
+++ negocio/ClienteNegocio.cs
@@ -0,0 +1,149 @@
+﻿using dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace negocio
+{
+    public class ClienteNegocio
+    {
+        //metodo para agregar un cliente a nuestra DB
+        public void agregarCliente(Cliente cliente) { 
+         AccesoDatos accesoDatos = new AccesoDatos();
+            try
+            {
+                accesoDatos.setConsulta("INSERT INTO Clientes (Documento,Nombre,Apellido,Email,Direccion,Ciudad,CP)\r\n  " +
+                                        "VALUES\r\n  " +
+                                        "(@dni,@nombre,@apellido,@email,@direccion,@ciudad,@cp)");
+                accesoDatos.setParametro("@dni",cliente.Dni);
+                accesoDatos.setParametro("@nombre",cliente.Nombre);
+                accesoDatos.setParametro("@apellido",cliente.Apellido);
+                accesoDatos.setParametro("@email",cliente.Email);
+                accesoDatos.setParametro("@direccion",cliente.Direccion);
+                accesoDatos.setParametro("@ciudad",cliente.Ciudad);
+                accesoDatos.setParametro("@cp",cliente.CodPostal);
+
+                accesoDatos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                accesoDatos.cerrarConexion();
+            }
+        }
+        public List<Cliente> getClientes()
+        {
+            AccesoDatos accesoDatos= new AccesoDatos();
+            List<Cliente> clientes = new List<Cliente> ();
+            try
+            {
+                accesoDatos.setConsulta("SELECT Id, Documento, Nombre, Apellido, Email, Direccion, Ciudad, CP FROM Clientes");
+                accesoDatos.ejecutarLectura();
+
+                while (accesoDatos.Lector.Read())
+                {
+                    Cliente cliente = new Cliente ();
+                    cliente.Id =(int) accesoDatos.Lector["Id"];
+                    cliente.Dni = (string)accesoDatos.Lector["Documento"];
+                    cliente.Nombre =(string) accesoDatos.Lector["Nombre"];
+                    cliente.Apellido = (string)accesoDatos.Lector["Apellido"];
+                    cliente.Email = (string)accesoDatos.Lector["Email"];
+                    cliente.Direccion = (string)accesoDatos.Lector["Direccion"];
+                    cliente.Ciudad = (string)accesoDatos.Lector["Ciudad"];
+                    cliente.CodPostal = (int)accesoDatos.Lector["CP"];
+
+                    clientes.Add(cliente);
+                }
+                return clientes;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                accesoDatos.cerrarConexion();
+            }
+        }
+        public Cliente GetClienteByDni(string dni)
+        {
+            AccesoDatos accesoDatos = new AccesoDatos();
+            Cliente cliente = new Cliente();
+            try
+            {
+                accesoDatos.setConsulta("SELECT Id, Documento, Nombre, Apellido, Email, Direccion, Ciudad, CP FROM Clientes WHERE Documento=@dni");
+                accesoDatos.setParametro("@dni", dni);
+                accesoDatos.ejecutarLectura();
+
+                while (accesoDatos.Lector.Read())
+                {
+                    
+                    cliente.Id = (int)accesoDatos.Lector["Id"];
+                    cliente.Dni =(string) accesoDatos.Lector["Documento"];
+                    cliente.Nombre = (string)accesoDatos.Lector["Nombre"];
+                    cliente.Apellido = (string)accesoDatos.Lector["Apellido"];
+                    cliente.Email = (string)accesoDatos.Lector["Email"];
+                    cliente.Direccion = (string)accesoDatos.Lector["Direccion"];
+                    cliente.Ciudad = (string)accesoDatos.Lector["Ciudad"];
+                    cliente.CodPostal = (int)accesoDatos.Lector["CP"];
+
+                    
+                }
+                return cliente;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                accesoDatos.cerrarConexion();
+            }
+        }
+        public Cliente GetClienteById(int id)
+        {
+            AccesoDatos accesoDatos = new AccesoDatos();
+            Cliente cliente = new Cliente();
+            try
+            {
+                accesoDatos.setConsulta("SELECT Id, Documento, Nombre, Apellido, Email, Direccion, Ciudad, CP FROM Clientes WHERE Id=@id");
+                accesoDatos.setParametro("@id", id);
+                accesoDatos.ejecutarLectura();
+
+                while (accesoDatos.Lector.Read())
+                {
+
+                    cliente.Id = (int)accesoDatos.Lector["Id"];
+                    cliente.Dni = (string)accesoDatos.Lector["Documento"];
+                    cliente.Nombre = (string)accesoDatos.Lector["Nombre"];
+                    cliente.Apellido = (string)accesoDatos.Lector["Apellido"];
+                    cliente.Email = (string)accesoDatos.Lector["Email"];
+                    cliente.Direccion = (string)accesoDatos.Lector["Direccion"];
+                    cliente.Ciudad = (string)accesoDatos.Lector["Ciudad"];
+                    cliente.CodPostal = (int)accesoDatos.Lector["CP"];
+
+
+                }
+                return cliente;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                accesoDatos.cerrarConexion();
+            }
+        }
+    }
+}
---
+++ negocio/VoucherNegocio.cs
@@ -0,0 +1,95 @@
+﻿using dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negocio
+{
+    public class VoucherNegocio
+    {
+        public List<Voucher> listar()
+        {
+            List<Voucher> vouchers = new List<Voucher>();
+            AccesoDatos datos = new AccesoDatos();
+
+
+            try
+            {
+                datos.setConsulta("SELECT CODIGOVOUCHER, FECHACANJE, IDCLIENTE, IDARTICULO  FROM VOUCHERS");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Voucher voucher = new Voucher();
+                    voucher.CodigoVoucher = (string)datos.Lector["CodigoVoucher"];
+                    if (!(datos.Lector["FechaCanje"] is DBNull))
+                        voucher.FechaCanje = (DateTime)datos.Lector["FechaCanje"];
+                    if (!(datos.Lector["IdCliente"] is DBNull))
+                        voucher.IdCliente = (int)datos.Lector["IdCliente"];
+                    else voucher.IdCliente = 0;
+
+                    if (!(datos.Lector["IdArticulo"] is DBNull))
+                        voucher.IdArticulo = (int)datos.Lector["IdArticulo"];
+                    else voucher.IdArticulo = 0;
+                    
+                    vouchers.Add(voucher);
+                }
+
+                return vouchers;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void asignarVoucher(Voucher voucher, int idCliente,int idArt)
+        {
+            AccesoDatos accesoDatos = new AccesoDatos();
+
+            try
+            {
+                accesoDatos.setConsulta("UPDATE VOUCHERS SET IDCLIENTE = @IdCliente, IDARTICULO = @IdArticulo, FechaCanje = GETDATE() WHERE CodigoVoucher = @CodigoVoucher ");
+                accesoDatos.setParametro("@CodigoVoucher", voucher.CodigoVoucher);
+                accesoDatos.setParametro("@IdCliente", idCliente);
+                accesoDatos.setParametro("@IdArticulo", idArt);
+                accesoDatos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                accesoDatos.cerrarConexion();
+            }
+        }
+
+        public string verificarVoucher(string codigoVoucher)
+        {
+            List<Voucher> listaVoucher = listar();
+
+            foreach (Voucher voucher in listaVoucher)
+            {
+                if (voucher.CodigoVoucher == codigoVoucher)
+                {
+                    if (voucher.IdCliente == 0 && voucher.IdArticulo == 0)
+                        return "Código válido";
+
+                    return "Código ya usado";
+                }
+            }
+
+            return "Código inválido";
+        }
+    }
+
+}
